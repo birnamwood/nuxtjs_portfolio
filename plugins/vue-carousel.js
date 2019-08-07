95c30c5be487aba5711537ea4d72800b574e6f98
birnamwood <birnamwood19@gmail.com>
2019-08-07 16:20:58 +0900

    vue-carouselの読み込み変更

--- plugins/vue-carousel.js
+++ plugins/vue-carousel.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
 import VueCarousel from 'vue-carousel'
+import Carousel from 'vue-carousel/src/Carousel.vue'
+import Slide from 'vue-carousel/src/Slide.vue'
 
 Vue.use(VueCarousel)
+Vue.use(Carousel)
+Vue.use(Slide)
