e14063650a0bf9624060c6ce906197875d47aa98
birnamwood <birnamwood19@gmail.com>
2019-08-07 13:58:24 +0900

    indexページ作成とヘッダー追加

---
+++ components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+<header>
+   <a href="/" class="title">Top</a>
+
+    <nav>
+      <a href="/" class="menu">Profile</a>
+      <a href="/" class="menu">History</a>
+      <a href="/" class="menu">Skill</a>
+      <a href="/" class="menu">Work</a>
+    </nav>
+  </header>
+</template>
+
+<script>
+import smoothScroll from 'vue-smoothscroll'
+
+export default {
+  methods: {
+
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+header {
+  top: 0;
+  position: fixed;
+  font-family: fantasy;
+  background-color: rgb(218, 226, 218,0.7);
+  width: 100%;
+  font-size: 24px;
+  text-align: center;
+  box-shadow: 0 3px 0 0 #b4afaf, 0 1px 1px 0 #e9e9e9;
+  transform: translateY(-3px);
+  padding: 1em 1em 0.5em;
+  color: #0f2018;
+  z-index: 9999;
+}
+
+.title {
+  float:left;
+  color: inherit;
+  text-decoration: none;
+}
+
+nav {
+  float: right;
+}
+
+.menu {
+  color: inherit;
+  text-decoration: none;
+  padding: 1em 1em 0.5em;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,28 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        portfolio
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+     <Header/>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import Header from '~/components/Header.vue'
 
 export default {
   components: {
-    AppLogo
-  }
+    Header,
+  },
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
 .container {
-  min-height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  flex-direction: column;
+  min-height: 100vh;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
-
